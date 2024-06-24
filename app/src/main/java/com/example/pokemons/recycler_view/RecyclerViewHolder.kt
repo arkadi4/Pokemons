28654cf65245ec2ec8deb3ebe566c6afe84ab433
arkadi4 <arkadi40102@gmail.com>
2024-06-24 10:16:11 +0300

    linear layout with fixes form previous pull request

--- app/src/main/java/com/example/pokemons/recycler_view/RecyclerViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.example.pokemons.recycler_view
-
-import androidx.recyclerview.widget.RecyclerView
-import com.example.pokemons.databinding.ItemOfRecyclerViewBinding
-
-class RecyclerViewHolder(val binding: ItemOfRecyclerViewBinding): RecyclerView.ViewHolder(binding.root) {
-}
